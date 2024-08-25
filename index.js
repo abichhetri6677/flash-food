46cc69873949b234365355b5fae3039b90267b0d
abichhetri6677 <150071392+abichhetri6677@users.noreply.github.com>
2024-08-25 18:10:43 +0545

    Add files via upload
    fghjk

---
+++ index.js
@@ -0,0 +1,19 @@
+// let x = document.getElementById('homesecs');
+// let roam = document.querySelector('.menucl');
+// let home = document.querySelector('.menu');
+
+
+
+// function openMyBar(){
+//     roam.style.display = "block"
+//     x.style.display = "block";
+//     home.style.display = "none"
+   
+// }
+
+// function closeMyBar(){
+//      roam.style.display = "none"
+//     x.style.display = "none";
+//     home.style.display = "block"
+   
+// }
